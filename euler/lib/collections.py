085e43a979a46c4f41d44cc08fd5790b37d53c4e
Evan Leis <evan.explodes@gmail.com>
2016-04-10 05:18:57 -0600

    [insert_in_order] add better documentation, use more verbose variable names

--- euler/lib/collections.py
+++ euler/lib/collections.py
@@ -1,11 +1,14 @@
-def insert_in_order(L, i, unique=True):
+def insert_in_order(L, item, unique=True):
     """
     Insert i into `L` in order with or without duplicates
+    :param L: list to add to
+    :param item: item to insert
+    :param unique: if true, do not add duplicates
     """
     for index in xrange(len(L)):
-        if unique and L[index] == i:
+        if unique and L[index] == item:
             return
-        if L[index] > i:
-            L.insert(index, i)
+        if L[index] > item:
+            L.insert(index, item)
             return
-    L.append(i)
+    L.append(item)

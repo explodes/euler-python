ad375c1165bccf9428b5abc7be3f4397398ae353
Evan Leis <evan.explodes@gmail.com>
2016-04-10 02:09:28 -0600

    [timing] add timing utility to guage optimizations

---
+++ euler/lib/timing.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+from time import time as t
+from functools import wraps
+
+
+def time(func):
+    """
+    Time a function.
+    Log the time, parameters, and result of a function.
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        sig = "%s(%s, %s)" % (func.__name__, args, kwargs)
+        print "→ %s" % sig
+        then = t()
+        result = func(*args, **kwargs)
+        now = t()
+        print "← %s :: %0.6fs ⇒ %s" % (sig, (now - then), result)
+        return result
+
+    return wrapper

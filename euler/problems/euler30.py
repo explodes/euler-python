302d6c6ac2381aac3260d086507bca6fe6904fe0
Evan Leis <evan.explodes@gmail.com>
2016-05-08 13:16:02 -0600

    [30] implemented 30

--- euler/problems/euler30.py
+++ euler/problems/euler30.py
@@ -2,15 +2,40 @@
 # coding=utf-8
 from euler.problems.registry import register
 
+LIMIT = 1000000
+
+
+def sum_of_powers(a, n):
+    s = 0
+    while a > 0:
+        rem = a % 10
+        s += rem ** n
+        a = (a - rem) / 10
+    return s
+
+
+def is_sum_of_powers(a, n):
+    return sum_of_powers(a, n) == a
+
+
+def is_sum_of_powers_gen(n, limit):
+    for i in xrange(2, limit + 1):
+        if is_sum_of_powers(i, n):
+            yield i
+
+
+def sum_of_sum_of_powers(n, limit):
+    return sum(is_sum_of_powers_gen(n, limit))
+
 
 @register
 class Euler:
     """
     Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
 
-    1634 = 14 + 64 + 34 + 44
-    8208 = 84 + 24 + 04 + 84
-    9474 = 94 + 44 + 74 + 44
+    1634 = 1^4 + 6^4 + 3^4 + 4^4
+    8208 = 8^4 + 2^4 + 0^4 + 8^4
+    9474 = 9^4 + 4^4 + 7^4 + 4^4
     As 1 = 14 is not a sum it is not included.
 
     The sum of these numbers is 1634 + 8208 + 9474 = 19316.
@@ -19,13 +44,22 @@ class Euler:
     """
     NUMBER = 30
     NAME = "Digit fifth powers"
-    ANSWER = None
+    ANSWER = 443839
 
     def run(self):
-        return "NOT IMPLEMENTED"
+        return sum_of_sum_of_powers(5, LIMIT)
 
     def test(self):
-        pass
+        assert sum_of_powers(1634, 4) == 1634
+        assert is_sum_of_powers(1634, 4)
+
+        assert sum_of_powers(8208, 4) == 8208
+        assert is_sum_of_powers(8208, 4)
+
+        assert sum_of_powers(9474, 4) == 9474
+        assert is_sum_of_powers(9474, 4)
+
+        assert sum_of_sum_of_powers(4, 10000)
 
 
 if __name__ == '__main__':

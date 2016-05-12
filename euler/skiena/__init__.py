17210c53f0be8494ff9381370dd07837b23f164a
Evan Leis <evan.explodes@gmail.com>
2016-05-11 20:08:33 -0600

    [skiena] add skiena package for practice from The Algorithm Design Manual 2nd Ed - Skiena

---
+++ euler/skiena/__init__.py
@@ -0,0 +1,7 @@
+"""
+In this package you will find exercises and algorithms from the book:
+
+The Algorithm Design Manual 2nd Edition - Steven S. Skiena
+Copyright 2010 Springer-Verlag London Limited
+ISB: 978-1-84996-720-4
+"""

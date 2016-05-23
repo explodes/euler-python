2ff1d22c5d55e599ec0f47fcab6f1e5297f8ced3
Evan Leis <evan.explodes@gmail.com>
2016-05-23 16:38:51 -0600

    [28] impl #28

--- euler/problems/euler28.py
+++ euler/problems/euler28.py
@@ -3,16 +3,32 @@
 from euler.problems.registry import register
 
 
+def spiral_gen(side):
+    if side == 0:
+        return
+
+    value = 1
+    accum = 2
+
+    yield 1
+    for ring in xrange(1, side / 2 + 1):
+        for i in xrange(4):
+            value += accum
+            yield value
+        accum += 2
+
+
 @register
 class Euler:
     """
     Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
-
-    [21]  22  23  24 [25]
-     20  [7]  8  [9]  10
-     19   6  [1]  2   11
-     18  [5]  4  [3]  12
-    [17]  16  15  14 [13]
+    [43] 44    45   46 47 48 [49]
+     42 [21]  22  23  24 [25] 26
+     41  20  [7]  8  [9]  10  27
+     40  19   6  [1]  2   11  28
+     39  18  [5]  4  [3]  12  29
+     38 [17]  16  15  14 [13] 30
+    [37] 36   35  34  33  32 [31]
 
     It can be verified that the sum of the numbers on the diagonals is 101.
 
@@ -20,13 +36,19 @@ class Euler:
     """
     NUMBER = 28
     NAME = "Number spiral diagonals"
-    ANSWER = None
+    ANSWER = 669171001
 
     def run(self):
-        return "NOT IMPLEMENTED"
+        return sum(spiral_gen(1001))
 
     def test(self):
-        pass
+        def test_spiral_gen(side, expected):
+            result = sum(spiral_gen(side))
+            assert result == expected, "sprial_gen_sum(%d) was %d not %d" % (side, result, expected)
+
+        test_spiral_gen(1, 1)
+        test_spiral_gen(3, 25)
+        test_spiral_gen(5, 101)
 
 
 if __name__ == '__main__':

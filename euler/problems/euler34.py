337969cc66e2faf1a301736417c697e10c3b5522
Evan Leis <evan.explodes@gmail.com>
2016-05-24 09:16:56 -0600

    [34] correct implementation

--- euler/problems/euler34.py
+++ euler/problems/euler34.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python
 # coding=utf-8
+from euler.lib.math import factorial
+from euler.lib.mem import memoize
 from euler.problems.registry import register
+
+LIMIT = 100000
+
+
+@memoize()
+def factorial_mem(n):
+    return factorial(n)
+
+
+def digits_of(n):
+    while n > 10:
+        d = n % 10
+        yield d
+        n = (n - d) / 10
+    yield n
+
+
+def is_curious(n):
+    return n == sum(factorial_mem(d) for d in digits_of(n))
+
+
+def curious_gen(limit):
+    for x in xrange(3, limit + 1):
+        if is_curious(x):
+            yield x
+
+
+def curious_sum(limit):
+    return sum(curious_gen(limit))
 
 
 @register
@@ -14,13 +45,23 @@ class Euler:
     """
     NUMBER = 34
     NAME = "Digit factorials"
-    ANSWER = None
+    ANSWER = 40730
 
     def run(self):
-        return "NOT IMPLEMENTED"
+        return curious_sum(LIMIT)
 
     def test(self):
-        pass
+        def test_digits_of(n, expected):
+            result = list(digits_of(n))
+            assert result == expected, 'digits_of(%d) == %s, not %s' % (n, result, expected)
+
+        test_digits_of(1, [1])
+        test_digits_of(12, [2, 1])
+        test_digits_of(123, [3, 2, 1])
+        test_digits_of(145, [5, 4, 1])
+
+        assert is_curious(145)
+        assert 145 in curious_gen(145)
 
 
 if __name__ == '__main__':

a67179623baa2bdce04ff77365d97b002e7d27f9
Evan Leis <evan.explodes@gmail.com>
2016-05-23 23:08:23 -0600

    [31] wrote a solution, found out why the solution was very, very wrong-- need to not double-count possibilities (todo)

--- euler/problems/euler31.py
+++ euler/problems/euler31.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
 # coding=utf-8
+
 from euler.problems.registry import register
+
+
+def num_comprising_combinations(value, options, mem=None):
+    if mem is None:
+        mem = {}
+    elif value in mem:
+        return mem[value]
+
+    num_variations = 0
+    for index, option in enumerate(options):
+        if value == option:
+            num_variations += 1
+            print "fullfilled", num_variations
+        elif value > option:
+            num_variations += num_comprising_combinations(value - option, options, mem)
+            print "exploded", num_variations
+
+    mem[value] = num_variations
+
+    print value, num_variations
+
+    return num_variations
 
 
 @register
@@ -19,13 +42,22 @@ class Euler:
     ANSWER = None
 
     def run(self):
-        return "NOT IMPLEMENTED"
+        return num_comprising_combinations(200, [1, 2, 5, 10, 20, 50, 100, 200])
 
     def test(self):
-        pass
+        def test_num_comprising_combinations(value, options, expected):
+            result = num_comprising_combinations(value, options)
+            assert result == expected, 'possibilites(%d, %s) was %d not %d' % (value, options, result, expected)
+
+        test_num_comprising_combinations(10, [], 0)
+        test_num_comprising_combinations(10, [11], 0)
+        test_num_comprising_combinations(10, [10], 1)
+        test_num_comprising_combinations(10, [5, 10], 2)  # 0,0 + 1
+        test_num_comprising_combinations(10, [5, 5, 10], 4)  # possible combos: 0,0 + 0,1 + 1,1 + 2 # NOT 1,0 also.
+        test_num_comprising_combinations(10, [1, 10], 2)
 
 
 if __name__ == '__main__':
     euler = Euler()
-    euler.test()
+    # euler.test()
     print euler.run()

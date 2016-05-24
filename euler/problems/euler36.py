607d4469e89186ac9024bb62ee4d5e8ef06cce51
Evan Leis <evan.explodes@gmail.com>
2016-05-24 09:44:42 -0600

    [36] implementation

--- euler/problems/euler36.py
+++ euler/problems/euler36.py
@@ -1,12 +1,37 @@
 #!/usr/bin/env python
 # coding=utf-8
+from euler.lib.strings import is_palindrome
 from euler.problems.registry import register
+
+LIMIT = 1000000
+
+
+def num_is_palindrome(n):
+    return is_palindrome(str(n))
+
+
+def binary_is_palindrom(n):
+    seq = bin(n)
+    seq = seq[2:]  # trim off leading 0b
+    return is_palindrome(seq)
+
+
+def is_double_palindromic(n):
+    return num_is_palindrome(n) and binary_is_palindrom(n)
+
+
+def palindrome_sums(limit):
+    s = 0
+    for n in xrange(1, limit):
+        if is_double_palindromic(n):
+            s += n
+    return s
 
 
 @register
 class Euler:
     """
-    The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+    The decimal number, 585 = 1001001001 (binary), is palindromic in both bases.
 
     Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
 
@@ -14,13 +39,15 @@ class Euler:
     """
     NUMBER = 36
     NAME = "Double-base palindromes"
-    ANSWER = None
+    ANSWER = 872187
 
     def run(self):
-        return "NOT IMPLEMENTED"
+        return palindrome_sums(LIMIT)
 
     def test(self):
-        pass
+        assert num_is_palindrome(585)
+        assert binary_is_palindrom(585)
+        assert is_double_palindromic(585)
 
 
 if __name__ == '__main__':
